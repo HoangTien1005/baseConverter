2144f9fef5d2f0a216170d02316de42f083da284
HoangTien1005 <nguyenhoangtien10052001@gmail.com>
2021-03-27 21:48:33 +0700

    khung code

--- baseConverter/baseConverter.h
+++ baseConverter/baseConverter.h
@@ -1,4 +1,4 @@
-#ifndef BASECONVERTER_H
+﻿#ifndef BASECONVERTER_H
 #define BASECONVERTER_H
 
 #include <iostream>
@@ -6,5 +6,21 @@
 
 using namespace std;
 
+										// chỉ có hệ nhị phân là không có dấu (-)
+class baseConverter {					// chuyển qua trung gian là thập phân
+private:
+	string input;						// chuỗi ng dùng nhập 
+	int base;
+	bool baseCheck();					// kiểm tra định dạng số
+	string toDecimal();					// trung gian qua thập phân
+	string flipBit();					// biểu diễn số âm hệ nhị phân ( bù 1 )
+	
 
+public:
+	void nhap();						// nhập input với base
+	void xuat();						// xuất kết quả
+	char getChar(int number);			// chuyển số sang ký tự 
+	int getNum(char ch);				// chuyển ký tự sang số
+	string DecimalToBase(int base);		// từ thập phân sang base ng dùng yêu cầu
+};
 #endif
